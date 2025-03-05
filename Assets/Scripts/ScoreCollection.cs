3ff10051d978d7f1f508b49c9976e261fe96d59c
Janna Lomibao <129775487+jannalmb00@users.noreply.github.com>
2025-03-05 17:19:44 -0500

    score collection

---
+++ Assets/Scripts/ScoreCollection.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCollection : MonoBehaviour
+{
+    private int score = 0;
+
+    private void OnTriggerEnter(Collider collider)
+}
